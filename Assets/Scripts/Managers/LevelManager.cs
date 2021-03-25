c022dec86c35372efef2948b38a658d73e85bee2
Lucas Augusto de Almeida <lucas.augusto.almeida@gmail.com>
2021-03-25 10:45:32 -0300

    Simple Menu
    Main Menu, loading of game and returning to main menu

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -65,4 +65,9 @@ public class LevelManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void OnMainMenuButtonClicked()
+    {
+        SceneManager.LoadScene("Boot");
+    }
 }
